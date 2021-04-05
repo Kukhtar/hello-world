a25573fffeef30aa03719cce02dc410421b56eb4
Ivan_Kukhtar <ivan_kukhtar@epam.com>
2021-04-05 12:12:11 +0300

    test

---
+++ gcp-metrics/src/main/java/ua/kukhtar/gcpmetrics/GcpMetricsApplication.java
@@ -0,0 +1,13 @@
+package ua.kukhtar.gcpmetrics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcpMetricsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcpMetricsApplication.class, args);
+	}
+
+}
---
+++ gcp-metrics/src/test/java/ua/kukhtar/gcpmetrics/GcpMetricsApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.kukhtar.gcpmetrics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcpMetricsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
